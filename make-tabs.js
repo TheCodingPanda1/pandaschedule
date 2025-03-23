881fafb1430261b466692de840fb6caaf109fd20
Glitch (glitch-blank-website) <none>
2025-03-23 04:18:06 +0000

    😼📇 Checkpoint ./style.css:68844431/559 ./index.html:68844431/138 ./make-tabs.js:68844431/91

--- make-tabs.js
+++ make-tabs.js
@@ -2,5 +2,7 @@ var tabs = document.getElementsByClassName("tab");
 var root = document.documentElement;
 for(var i = 0; i < tabs.length; i ++){
   var tabWidth = window.innerWidth / tabs.length;
+  var tabSelected
   tabs[i].style.left = i * tabWidth + "px";
+  tabs[i].style.width = tabWidth + "px";
 }
