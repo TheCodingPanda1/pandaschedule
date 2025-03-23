2fb707e03a935ce8d9f1fe6a8545e5fffcef037b
Glitch (glitch-blank-website) <none>
2025-03-23 04:29:17 +0000

    🌝🍫 Checkpoint ./make-tabs.js:68844431/603 ./style.css:68844431/939

--- make-tabs.js
+++ make-tabs.js
@@ -2,7 +2,7 @@ var tabs = document.getElementsByClassName("tab");
 var root = document.documentElement;
 for(var i = 0; i < tabs.length; i ++){
   var tabWidth = window.innerWidth / tabs.length;
-  var tabSelected
+  var tabSelected = tabs[i].getElementsByTagName("div")[0];
+  tabSelected.style.width = tabWidth + "px";
   tabs[i].style.left = i * tabWidth + "px";
-  tabs[i].style.width = tabWidth + "px";
 }
