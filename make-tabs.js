782f9e020032cd583cbd393fa105781e4d194975
Glitch (glitch-blank-website) <none>
2025-03-23 04:40:23 +0000

    🦁🛋 Checkpoint ./make-tabs.js:68844431/621 ./style.css:68844431/569

--- make-tabs.js
+++ make-tabs.js
@@ -1,8 +1,12 @@
 var tabs = document.getElementsByClassName("tab");
 var root = document.documentElement;
+var slider = document.getElementById("slider");
 for(var i = 0; i < tabs.length; i ++){
   var tabWidth = window.innerWidth / tabs.length;
-  var tabSelected = tabs[i].getElementsByTagName("div")[0];
-  tabSelected.style.width = tabWidth + "px";
   tabs[i].style.left = i * tabWidth + "px";
+  root.style.setProperty("--tab-width", tabWidth + "px");
+  tabs[i].addEventListener("click", function(){
+    slider.style.left = tabWidth * i + "px";
+    console.log(tabs[i]);
+  });
 }
