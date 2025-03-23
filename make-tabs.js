ae797a6c819d2a38ac23c29472d5417e02ddd875
Glitch (glitch-blank-website) <none>
2025-03-23 05:13:58 +0000

    🌺👡 Checkpoint ./make-tabs.js:68844431/178 ./style.css:68844431/209 ./index.html:68844431/294

--- make-tabs.js
+++ make-tabs.js
@@ -17,16 +17,15 @@ for(var i = 0; i < tabs.length; i ++){
   });
 };
 window.addEventListener("resize", function(){
+  var tabWidth = window.innerWidth / tabs.length;
   for(var j = 0; j < tabs.length; j ++){
     var tab = tabs[j];
     var tabInput = tab.getElementsByTagName("input")[0];
     if(tabInput.checked){
-      slider.style.left = tab.style.left;
-      console.log(tab.style.left);
+      slider.style.left = tabWidth * j + "px";
     }
   }
   for(var i = 0; i < tabs.length; i ++){
-    var tabWidth = window.innerWidth / tabs.length;
     tabs[i].style.left = i * tabWidth + "px";
     root.style.setProperty("--tab-width", tabWidth + "px");
     tabs[i].addEventListener("click", function(){
